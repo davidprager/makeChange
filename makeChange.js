543052cf765d1146734e7c2fc258567c8ae5340f
davidprager <david_prager@yahoo.com>
2021-08-26 20:36:25 -0400

    Initial commit of Project

---
+++ makeChange.js
@@ -0,0 +1,15 @@
+/* This program can be used to calculate the coins needed to make change */
+
+"use strict"
+
+function makeChange() {
+    const COIN_VALUES = [1, 5, 10, 25, 50, 100];
+    const COIN_NAMES = ["penny", "nickle", "dime", "quarter", "half-dollar", "dollar"];
+
+    let costAmount = 5.63;
+    let amountGiven = 10.00;
+
+}
+
+
+
